ecccd21da1b07fd45d5b2290d5e0365a389a4029
elaine <eshannon3@stmarytx.edu>
2019-06-18 13:43:46 -0500

    Step 2: add CSS and an attribute

--- src/block/block.js
+++ src/block/block.js
@@ -1,3 +1,6 @@
+import './style.scss';
+import './editor.scss';
+ 
 const registerBlockType = wp.blocks.registerBlockType;
 const { Dashicon } = wp.components;
  
@@ -5,10 +8,41 @@ registerBlockType("cgb/a11y-table", {
     title: 'A11y Table',
     icon: 'screenoptions',
     category: 'common',
-    edit: props => {
-        return <table><tbody><tr><td>Cell</td></tr></tbody></table>;
+    attributes: {
+        showTable: {
+            type: 'boolean',
+            default: false
+        }
     },
+    //////////////////// EDIT ////////////////////
+    edit: props => {
+        const { attributes: { showTable }, className, setAttributes } = props;
+        let formClass = '';
+        if(showTable) { formClass = 'is-hidden'; }
+        return (
+            <div>
+                <table className={ className }>
+                    <tbody>
+                    </tbody>
+                </table>
+                <form className={ formClass }>
+                    <button
+                        type='submit'
+                        onClick={evt => buildTable(evt) }
+                    >
+                        Insert table
+                    </button>
+                </form>
+            </div>
+        );
+        function buildTable(evt) {
+            evt.preventDefault();
+            props.setAttributes({ showTable: true });
+        }
+    },
+    //////////////////// SAVE ////////////////////
     save: props => {
+        const { className } = props;
         return <table><tbody><tr><td>Cell</td></tr></tbody></table>;
     }
 });
