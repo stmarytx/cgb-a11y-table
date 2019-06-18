a90d8723f03495c7e2ef2b98977e27d6dbf6f721
elaine <eshannon3@stmarytx.edu>
2019-06-18 13:59:59 -0500

    Step 4: fix the cursor; add captions and footers

--- src/block/block.js
+++ src/block/block.js
@@ -31,6 +31,16 @@ registerBlockType("cgb/a11y-table", {
                 }
             }
         },
+        dataCaption: {
+            type: 'string',
+            source: 'text',
+            selector: 'caption'
+        },
+        dataFooter: {
+            type: 'string',
+            source: 'text',
+            selector: 'tfoot td'
+        },
         numCols: {
             type: 'string',
             default: '2'
@@ -42,17 +52,41 @@ registerBlockType("cgb/a11y-table", {
         showTable: {
             type: 'boolean',
             default: false
+        },
+        useCaption: {
+            type: 'boolean',
+            default: true
+        },
+        useFooter: {
+            type: 'boolean',
+            default: false
         }
     },
     //////////////////// EDIT ////////////////////
     edit: props => {
-        console.log('Edit Attributes: ',props.attributes);
-        const { attributes: { dataBody, showTable }, className, setAttributes } = props;
+        const { attributes: { dataBody, dataCaption, dataFooter, showTable, useCaption, useFooter }, className, setAttributes } = props;
         let numCols = parseInt(props.attributes.numCols, 10);
         let numRows = parseInt(props.attributes.numRows, 10);
-        let formClass = '';
-        if(showTable) { formClass = 'is-hidden'; }
-        let tableBody, tableBodyData = dataBody
+        // Caption
+        let tableCaption, captionClass = 'is-hidden';
+        if(showTable) {
+            captionClass = '';
+        }
+        if(useCaption) {
+            tableCaption = <caption
+                className={ captionClass }
+                contenteditable='true'
+            >
+                { dataCaption }
+            </caption>;
+            tableCaption.props.onInput = function(evt) {
+                props.setAttributes({ dataCaption: evt.target.textContent });
+                // Move the cursor back where it was
+                setCursor(evt);
+            };
+        }
+        // Table Body
+        let tableBody, formClass = '', tableBodyData = dataBody
         .map(function(rows, rowIndex) {
             let rowCells = rows.bodyCells.map(function(cell, colIndex) {
                 let cellOptions = {
@@ -68,6 +102,8 @@ registerBlockType("cgb/a11y-table", {
                         props.setAttributes({
                             dataBody: newBody
                         });
+                        // Move the cursor back where it was
+                        setCursor(evt);
                     }
                 };
                 let currentBodyCell = el(
@@ -82,12 +118,46 @@ registerBlockType("cgb/a11y-table", {
         if(tableBodyData.length) {      
             tableBody = <tbody>{ tableBodyData }</tbody>;
         }
+        // Table Footer
+        var tableFooter, footerClass = 'is-hidden';
+        if(showTable) {
+            footerClass = '';
+            formClass = 'is-hidden';
+        }
+        if(useFooter == true) {
+            let tableFooterTd = <td
+                colspan={ numCols }
+                className={ footerClass }
+                contenteditable='true'
+            >
+                { dataFooter }
+            </td>;
+            tableFooterTd.props.onInput = function(evt) {
+                props.setAttributes({ dataFooter: evt.target.textContent });
+                // Move the cursor back where it was
+                setCursor(evt);
+            };
+            tableFooter = <tfoot><tr>{ tableFooterTd }</tr></tfoot>;
+        }
         return (
             <div>
                 <table className={ className }>
+                    { tableCaption }
                     { tableBody }
+                    { tableFooter }
                 </table>
                 <form className={ formClass }>
+                    <div>
+                        <label for='addCaption'>{ __('Add Caption') }</label>
+                        <input
+                            type='checkbox'
+                            id='captionCheck'
+                            checked={ useCaption }
+                            onChange={ function(evt) {
+                                props.setAttributes({ useCaption: evt.target.checked });
+                            }}
+                        />
+                    </div>
                     <div>
                         <label for='numCols'>{ __('Columns') }</label>
                         <input
@@ -110,6 +180,17 @@ registerBlockType("cgb/a11y-table", {
                             step='1'
                             pattern='[0-9]*'
                             onChange={ (evt) => props.setAttributes({ numRows: evt.target.value }) }
+                        />
+                    </div>
+                    <div>
+                        <label for='addFooter'>{ __('Add Footer') }</label>
+                        <input
+                            type='checkbox'
+                            id='footerCheck'
+                            checked={ useFooter }
+                            onChange={ function(evt) {
+                                props.setAttributes({ useFooter: evt.target.checked });
+                            }}
                         />
                     </div>
                     <button
@@ -141,13 +222,33 @@ registerBlockType("cgb/a11y-table", {
                 });
             }
         }
+        // Function that returns the cursor where it was, instead of the beginning of an input
+        function setCursor(evt) {
+            var node = evt.target;
+            var caret = window.getSelection().anchorOffset;
+            if(node.firstChild) {
+                setTimeout(function() {
+                    let textNode = node.firstChild;
+                    var range = document.createRange();
+                    range.setStart(textNode, caret);
+                    range.setEnd(textNode, caret);
+                    var sel = window.getSelection();
+                    sel.removeAllRanges();
+                    sel.addRange(range);
+                }, 1, node, caret);
+            }
+        }
     },
     //////////////////// SAVE ////////////////////
     save: props => {
-        const { attributes: { dataBody }, className } = props;
+        const { attributes: { dataBody, dataCaption, dataFooter, useCaption, useFooter }, className } = props;
         let numCols = parseInt(props.attributes.numCols, 10);
         let numRows = parseInt(props.attributes.numRows, 10);
-        // Table Body
+        // Caption
+        var tableCaption;
+        if(useCaption === true) {
+            tableCaption = <caption>{ dataCaption }</caption>
+        }
         let tableBody, tableBodyData = dataBody
         .map(function(rows) {
             let rowCells = rows.bodyCells.map(function(cell) {
@@ -163,9 +264,16 @@ registerBlockType("cgb/a11y-table", {
         if(tableBodyData.length) {      
             tableBody = <tbody>{ tableBodyData }</tbody>;
         }
+        // Table Footer
+        let tableFooter;
+        if(useFooter == true) {
+            tableFooter = <tfoot><tr><td colspan={ numCols }>{ dataFooter }</td></tr></tfoot>
+        }
         return (
             <table className={ className }>
+                { tableCaption }
                 { tableBody }
+                { tableFooter }
             </table>
         );
     }
